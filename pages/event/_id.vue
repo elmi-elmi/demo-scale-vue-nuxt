bb75d3931303f33c2621994f95161269671dede2
elmi-elmi <shahrokhelmi@gmail.com>
2022-05-03 18:15:49 +0430

    l6-start

---
+++ pages/event/_id.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    Event # {{ this.id }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "_id",
+  head() {
+    return {
+      title: `#${this.id} event  title + `,
+      meta:[
+        {
+          hid:'description',
+          name:'description',
+          content:`content of event #${this.id}`
+        }
+      ]
+    }
+  },
+  computed: {
+    id() {
+      return this.$route.params.id
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/event/create.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <h1>
+    Create Event
+  </h1>
+</div>
+</template>
+
+<script>
+export default {
+  head(){
+    return{
+      title:'title from create.vue',
+      meta:[
+        {
+          hid:'description',
+          name:'description',
+          content:'create page content for seo'
+        }
+      ]
+    }
+  },
+  name: "create"
+}
+</script>
+
+<style scoped>
+
+</style>
