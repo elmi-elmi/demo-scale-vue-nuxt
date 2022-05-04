4d42c31888ef194eb6c81358316901b4681694f0
elmi-elmi <shahrokhelmi@gmail.com>
2022-05-04 19:04:05 +0430

    l7-start

--- pages/event/_id.vue
+++ pages/event/_id.vue
@@ -1,20 +1,22 @@
 <template>
-  <div>
-    Event # {{ this.id }}
-  </div>
+  <h1>
+     {{ this.event.title }}
+  </h1>
 </template>
 
 <script>
+import EventService from "@/services/EventService";
+
 export default {
   name: "EventUser",
   head() {
     return {
-      title: `#${this.id} event  title + `,
+      title: this.event.title,
       meta:[
         {
           hid:'description',
           name:'description',
-          content:`content of event #${this.id}`
+          content:`content of event #${this.event.id}`
         }
       ]
     }
@@ -23,6 +25,21 @@ export default {
     id() {
       return this.$route.params.id
     }
+  },
+  async asyncData({ error, params}) {
+    try{
+
+      const {data}= await EventService.getEvent(params.id)
+      return {
+        event: data
+      }
+    }catch(e){
+      error({
+        statusCode:503,
+        message:'Unabel to fetch event #'+params.id
+      })
+    }
+
   }
 }
 </script>
